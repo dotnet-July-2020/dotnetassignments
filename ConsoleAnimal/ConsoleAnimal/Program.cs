924bb37066de8211fb52ca2ab2b98a2d38c5d392
Geethika <geethika.duddupudi@zensar.com>
2020-08-03 20:44:45 +0530

    Add loop statements

---
+++ ConsoleAnimal/ConsoleAnimal/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] MyArray = { "Zebra", "Ant", "Owl", "Lion", "Dog" };
+            Console.WriteLine("--------For Loop----------");
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                Console.WriteLine(MyArray[i]);
+                
+            }
+            Console.WriteLine("--------Foeach Loop------");
+            foreach(string i in MyArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("----------dowhile loop-------");
+            int j= 0;
+            do
+            {
+
+
+                Console.WriteLine(MyArray[j]);
+                j++;
+            } while (j < MyArray.Length);
+            Console.WriteLine("--------while loop------");
+            int k = 0;
+            while (k< MyArray.Length)
+            {
+                Console.WriteLine(MyArray[k]);
+                k++;
+            }
+            string[] Animals = { "Zebra", "Kiwi", "Amazona Parrot", "Tiger" };
+            string[] Places = { "Africa", "New Zealand", "Jamaica", "India" };
+            Array.Sort(Animals);
+            Console.WriteLine("----------Sorted array of animals-------");
+            foreach(string i in Animals)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("----------Sorted array of Places-------");
+            Array.Sort(Places);
+            foreach (string i in Places)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+        
+    }
+}
